c60f157b4c56b556d078e4440bd61f365ca4f78e
janderson22-creator <jandersomcosta@yahoo.com.br>
2025-01-30 18:11:11 -0300

    implement Mercado Pago payment integration with checkout and webhook handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import useMercadoPago from "./hooks/useMercadoPago";
+
 export default function Home() {
+  const { createMercadoPagoCheckout } = useMercadoPago();
   return (
-    <div className="flex items-center justify-center border border-blue-500 h-[100vh]">
-      <button className="bg-yellow-400 w-fit rounded-lg px-4 py-2 text-sm font-normal">
-        PAGAR
+    <div className="flex justify-center items-center h-screen">
+      <button
+        onClick={() =>
+          createMercadoPagoCheckout({
+            testeId: "123",
+            userEmail: "loveyuuqr@gmail.com",
+          })
+        }
+        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+      >
+        Comprar
       </button>
     </div>
   );
