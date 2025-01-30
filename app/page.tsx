9e5e6724665e34684abc97de09c246d4941f3fa8
janderson22-creator <jandersomcosta@yahoo.com.br>
2025-01-30 17:19:05 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center border border-blue-500 h-[100vh]">
+      <button className="bg-yellow-400 w-fit rounded-lg px-4 py-2 text-sm font-normal">
+        PAGAR
+      </button>
+    </div>
+  );
+}
